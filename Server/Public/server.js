d7ec9804bbbb0e70efebd20b0f957712ea099dc4
Katherine Daugherty <katherinedaugherty23@gmail.com>
2021-10-08 16:57:37 -0500

    folder organization

--- Server/Public/server.js
+++ Server/Public/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const PORT = 50000;
+const PORT = 5000;
 
 
 //required for GET and POST routes
