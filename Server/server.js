12e234f2505278945019eb9a35d645b1a627092b
Katherine Daugherty <katherinedaugherty23@gmail.com>
2021-10-09 12:35:24 -0500

    added HTML for inputs, buttons, and hardcoded layout

--- Server/server.js
+++ Server/server.js
@@ -3,16 +3,14 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = 5000;
 
-
 //required for GET and POST routes
 app.use(bodyParser.urlencoded({extended:true}));
-
 
 //server up static files (HTML, CSS, Client JS )
 app.use(express.static("server/public"));
 
-//Get & Post Routes 
-
 app.listen(PORT,() => {
     console.log('Server is running on port', PORT);   
 });
+
+//Get & Post Routes 
