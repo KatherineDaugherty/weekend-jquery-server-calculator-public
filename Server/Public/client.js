1cd482cd519def63021ded8fbcff343670c371d8
Katherine Daugherty <katherinedaugherty23@gmail.com>
2021-10-09 13:18:36 -0500

    POST on client side js

--- Server/Public/client.js
+++ Server/Public/client.js
@@ -1,6 +1,37 @@
 $(onReady);
 
 function onReady() {
-console.log('jquery is loaded ');
+    console.log('jquery is loaded ');
+//click handler
+$(`#submit`).on(`click`, sendInputs);
+}
 
+//send POST with input values 
+function sendInputs() {
+    $.ajax({
+        method: "POST",
+        url: "/submission",
+        data: {
+            data:
+            {
+                num1: $("#numInput").val(),
+                num2: $("#numInputTwo").val(),
+            }
+        }
+    }).then(function (response) {
+        // renderHistory();
+        $("#numInput").val('');
+        $("#numInputTwo").val('');
+    });
+}
+
+// GET array of inputHistory
+function renderHistory() {
+    $.ajax({
+        method: "GET",
+        url: "/submission",
+    }).then(function (response) {
+        console.log(response);
+
+    })
 }
