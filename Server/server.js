cbc0c2f262c60485ff7319e695e42b640a1aaa16
Katherine Daugherty <katherinedaugherty23@gmail.com>
2021-10-09 12:53:17 -0500

    server.js POST and GET, tested with postman

--- Server/server.js
+++ Server/server.js
@@ -13,4 +13,32 @@ app.listen(PORT,() => {
     console.log('Server is running on port', PORT);   
 });
 
-//Get & Post Routes 
+//hardcode array of objects data. 
+let inputHistory = [];
+
+let inputData = {num1: `2`,
+equation: `*` ,
+num2: `2`};
+
+let solution = 0;
+
+function doMath(request){
+    let data = request.data;
+    
+
+    inputHistory.push(inputData);
+    console.log(inputHistory);
+    
+}
+
+
+//GET and POST 
+app.post("/submission", (req, res) => {
+    console.log(req.body);
+    // doMath (req.body);
+    res.sendStatus(201);
+});
+
+app.get("/submission", (req, res) => {
+    res.send(inputHistory);
+});
