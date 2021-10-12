635f591c32052604130d90b59c2782ba0f0bcd16
Katherine Daugherty <katherinedaugherty23@gmail.com>
2021-10-12 11:36:39 -0500

    code change to add environment variable port

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 //required for GET and POST routes
 app.use(bodyParser.urlencoded({ extended: true }));
